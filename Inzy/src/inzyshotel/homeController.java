954cfe7d054e2b0c27554a429c864aa1b53104b4
Inas <inaschair@gmail.com>
2020-01-23 19:37:52 +0100

    inzy

---
+++ Inzy/src/inzyshotel/homeController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inzyshotel;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author CHAIR Inas
+ */
+public class homeController implements Initializable {
+
+    @FXML
+    private AnchorPane bp;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void home(MouseEvent event) {
+    }
+
+    @FXML
+    private void reservation(MouseEvent event) {
+    }
+
+    @FXML
+    private void clients(MouseEvent event) {
+    }
+
+    @FXML
+    private void rooms(MouseEvent event) {
+    }
+
+    @FXML
+    private void add_service(MouseEvent event) {
+    }
+
+    @FXML
+    private void statistics(MouseEvent event) {
+    }
+
+    @FXML
+    private void about_us(MouseEvent event) {
+    }
+    
+}
