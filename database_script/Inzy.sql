c4ccede80a5fa1dd789dbd8decb9351327dff99a
Inas <inaschair@gmail.com>
2020-01-25 20:10:40 +0100

    modified:   database_script/Inzy.sql

--- database_script/Inzy.sql
+++ database_script/Inzy.sql
@@ -1,9 +1,9 @@
 
 INSERT INTO `services` (`id`, `nom`, `price`, `state`, `description`) VALUES 
-(1,'SPA',150,'Salade', 0), 
-(2,'Sport Club',150,'Salade', 0), 
-(3,'Festival hall',150,'Salade', 0), 
-(4,'Conference hall',150,'Salade', 0), 
+(1,'SPA',150,'good', 'good'), 
+(2,'Sport Club',150,'good', 'good'), 
+(3,'Festival hall',150,'good', 'good'), 
+(4,'Conference hall',150,'good', 'good'), 
 
 
 INSERT INTO `rooms` (`id`, `floor`, `type`, `beds`, `description`,`price`,`state`) VALUES 
