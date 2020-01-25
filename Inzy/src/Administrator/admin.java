60fe729d23de6399e6e6d94ed4e71807f3976cc5
Inas <inaschair@gmail.com>
2020-01-25 19:55:54 +0100

    model added

--- Inzy/src/Administrator/admin.java
+++ Inzy/src/Administrator/admin.java
@@ -1,5 +1,5 @@
 package Administrator;
-import cashier.*;
+import Cashier.*;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
